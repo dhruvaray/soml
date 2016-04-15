2a1fc65832dbdda6f4497a1203a7f81b849a5539
Dhruva Ray <dhruvaray@gmail.com>
2016-04-15 22:26:07 +0530

    refactored the code to allow convert other entities

--- convertp2csv.py
+++ convertp2csv.py
@@ -3,37 +3,42 @@ import xml.etree.ElementTree as ET
 from collections import OrderedDict
 import base64
 
-ptree = ET.parse('Posts.xml')
 postcols = ['PostTypeId','AcceptedAnswerId','CreationDate','Score','ViewCount','Body','OwnerUserId','LastEditorUserId','LastEditDate','LastActivityDate','Title','Tags','AnswerCount','CommentCount','FavoriteCount']
+commentcols = ['Id','PostId','Score','Text','CreationDate','UserId']
 
-def posts(post):
+def documents(post,cols):
     for doc in post.iterfind('.//row'):
         row = OrderedDict()
-        for c in postcols:
+        for c in cols:
             if doc.attrib.get(c) is None:
                 row[c] = ''
             else:
                 val = doc.attrib[c].encode('utf-8')
-                if c in ['Body','Title']:
+                if c in ['Body','Title','Text']:
                     val = base64.b64encode(val)
                 row[c] = val.replace('\n','').replace('\r','').replace('\r\n','')
         yield row
 
+
+def xml2csv(root, type, headers):
+    with open(type +'.csv','w') as f:
+        f.write(','.join(headers) + '\n')
+        for count, p in enumerate(documents(root,headers)):
+            print 'record #' + `count`
+            f.write(','.join(p.values()) + '\n')
+
+    print '~~~ Sample file generation ~~~\n'
+
+    with open(type + '.sample.csv','w') as f:
+        f.write(','.join(headers) + '\n')
+        for count, p in enumerate(documents(root,headers)):
+            f.write(','.join(p.values()) + '\n')
+            if count > 100:
+                break
+
 print '~~~ xml -> csv ~~~\n'
+#ptree = ET.parse('Posts.xml')
+#xml2csv(ptree,'posts',postcols)
 
-with open('posts.csv','a') as f:
-    f.write(','.join(postcols) + '\n')
-    for count, p in enumerate(posts(ptree)):
-        print 'record #' + `count`
-        f.write(','.join(p.values()) + '\n')
-
-print '~~~ Sample file generation ~~~\n'
-
-with open('posts.sample.csv','w') as f:
-    f.write(','.join(postcols) + '\n')
-    for count, p in enumerate(posts(ptree)):
-        f.write(','.join(p.values()) + '\n')
-        if count > 100:
-            break
-
-
+ptree = ET.parse('Comments.xml')
+xml2csv(ptree,'comments',commentcols)

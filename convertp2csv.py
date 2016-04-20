97802f909e03eaf17defddd53e4e1c8420804b8f
Dhruva Ray <dhruvaray@gmail.com>
2016-04-20 10:14:21 +0530

    moving to stream based processing for xml data

--- convertp2csv.py
+++ convertp2csv.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import xml.etree.ElementTree as ET
 from collections import OrderedDict
 import base64
+
+import xml.etree.cElementTree as ET
 
 columns = {}
 columns['posts'] = ['Id','PostTypeId','ParentId','AcceptedAnswerId','CreationDate','Score','ViewCount','Body','OwnerUserId','LastEditorUserId','LastEditorDisplayName','LastEditDate','LastActivityDate','CommunityOwnedDate','ClosedDate','Title','Tags','AnswerCount','CommentCount','FavoriteCount']
@@ -12,31 +13,40 @@ columns['users'] = ['Id','Reputation','CreationDate','DisplayName','EmailHash','
 textcols =  ['Body','Title','Text','AboutMe','Location','Comment']
 
 #TODO : Stream v/s load whole document
-def documents(post,cols):
-    for doc in post.iterfind('.//row'):
-        row = OrderedDict()
-        for c in cols:
-            if doc.attrib.get(c) is None:
-                row[c] = ''
-            else:
-                val = doc.attrib[c].encode('utf-8')
-                if c in textcols:
-                    val = base64.b64encode(val)
-                row[c] = val.replace('\n','').replace('\r','').replace('\r\n','')
-        yield row
+def documents(context,cols):
 
+    for event, elem in context:
+        tag = elem.tag
+
+        if event == 'start' and tag == "row":
+            doc = elem.attrib
+            row = OrderedDict()
+            for c in cols:
+                if doc.get(c) is None:
+                    row[c] = ''
+                else:
+                    val = doc[c].encode('utf-8')
+                    if c in textcols:
+                        val = base64.b64encode(val)
+                    row[c] = val.replace('\n','').replace('\r','').replace('\r\n','')
+            elem.clear()        
+            yield row
 
 def xml2csv(root, type, headers):
     with open(type +'.csv','w') as f:
         f.write(','.join(headers) + '\n')
+        tc = 0
+        print '...',
         for count, p in enumerate(documents(root,headers)):
-            print 'record #' + `count`
+            tc = count
             f.write(','.join(p.values()) + '\n')
+        print tc
 
-#sources = ['Posts','Comments','PostHistory','Users']
-sources = ['PostHistory']
+sources = ['Posts','Comments','PostHistory','Users']
+#sources = ['Users']
 
 for source in sources:
-    print 'processing - ' + source
-    root = ET.parse(source + '.xml')
+    print 'processing : ' + source
+    root = iter(ET.iterparse(source + '.xml', events=("start", "end")))
     xml2csv(root,source.lower(),columns[source.lower()])
+
